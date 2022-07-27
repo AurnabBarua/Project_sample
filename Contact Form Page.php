c3f36e59004137a8639992054bd8aaf50cb0830f
Aurnab Barua <190204009@aust.edu>
2022-07-28 02:39:26 +0600

    updated_27_july

---
+++ Contact Form Page.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Contact Form</title>
+    <link type="text/css" rel="stylesheet" href="./css/mycss.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+
+<div class="container mt-5 bg-white p-5">
+  <h1>Contact Me!</h1>
+  <form class="row g-3" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <div class="col-md-6 p-1 bg-dark">
+      <label for="FirstName" class="form-label text-white">First Name</label>
+      <input type="text" name="f_name" class="form-control" id="FirstName" required>
+    </div>
+    <div class="col-md-6 bg-dark p-1">
+      <label for="LastName" class="form-label text-white">Last Name</label>
+      <input type="text" name="l_name" class="form-control" id="LastName" required>
+    </div>
+    <div class="col-md-8 bg-dark p-1">
+        <label for="emailinfo" class="form-label text-white">Email</label>
+      <input type="email" name="email" class="form-control" id="emailinfo" placeholder="abc@gmail.com" required>
+    </div>
+    <div class="col-md-4 bg-dark p-1">
+      <label for="phoneNumber" class="form-label text-white">Phone Number</label>
+      <input type="text" class="form-control" id="phoneNumber" placeholder="+880-**********">
+    </div>
+    <div class="col-md-12 bg-dark p-1">
+        <label for="comment" class="form-label text-white">Comment, Question?</label>
+      <textarea class="form-control" name="message" id="comment"rows="3"></textarea>
+    </div>
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-info">Submit</button>
+    </div>
+
+  </form>
+
+</div>
+
+
+<?php
+error_reporting(E_ERROR | E_PARSE);
+  $f_name = $_POST['f_name'];
+  $l_name = $_POST['l_name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = "my website";
+  $email_subject = "New form submitted";
+
+  $email_body = "User Name: $f_name $l_name.\n User Email: $visitor_email. \n User Message: $message.\n";
+
+  $to = "190204009@aust.edu";
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-to: $visitor_email \r\n";
+  mail($to, $email_subject, $email_body, $headers); 
+
+ ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ Data Listing Page.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>aurnab restaurent</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="/css/mycss.css">
+  </head>
+  <body>
+    <section class="text-center bg-danger">
+      <h2>OUR ALL ITEMS</h2>
+    </section>
+
+
+    <section class="bg-dark p-2">
+      <div class="container">
+        <div class="row g-4">
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_chicken_steak.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">CHICKEN STEAK</h3>
+               <p>Chicken Steak</p>
+               <p>BBQ Rice</p>
+               <p>Sauteed Mashroom</p>
+               <p>Sauteed Vegetable</p>
+               <p class="text-info"><b>487 taka</b></p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_lemon_chicken.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">LEMON CHICKEN</h3>
+               <p>Lemon chicken</p>
+               <p>Mashed potato</p>
+               <p>Sliced salad</p>
+               <p>Wedges</p>
+               <p class="text-info"><b>554 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_lobster_thermidor.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">LOBSTER THERMIDOR</h3>
+               <p>Lobster</p>
+               <p>BBQ Rice</p>
+               <p>Wedges</p>
+               <p class="text-info"><b>2487 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_mongolian_chicken.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">MONGOLIAN CHICKEN STEAK</h3>
+               <p>Mongolian Chicken Steak</p>
+               <p>Masked Potato</p>
+               <p>Sauteed Vegetable</p>
+               <p class="text-info"><b>490 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/1280px-Hyderabadi_Chicken_Biryani.jpg"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">HYDERABADI CHICKEN BIRYANI</h3>
+               <p>Hyderabadi Chicken Biryani</p>
+               <p>Basmoti Rice</p>
+               <p>Chicken</p>
+               <p class="text-info"><b>200 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-6 col-lg-3">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/Kacchi-Biryani-1-960x540.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">KACCHI BIRYANI</h3>
+               <p>Kacchi Biryani</p>
+               <p>Basmoti Rice</p>
+               <p>Boiled Egg</p>
+               <p>Salad</p>
+               <p class="text-info"><b>250 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+       </div>
+      </div>
+    </section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
